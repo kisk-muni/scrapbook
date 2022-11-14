a37dd37bfa122365ac57b596226e8daa058c7890
cernockyd <24832873+cernockyd@users.noreply.github.com>
2022-11-14 15:12:41 +0100

    use env variables for api url and anon token

--- lib/hooks/use-portfolios-list.ts
+++ lib/hooks/use-portfolios-list.ts
@@ -10,7 +10,7 @@ export interface PostItemProps {
 
 export default function usePortfoliosList() {
   return useApiInfinite<PostItemProps>(
-    `https://kiggmvgmzoffneyfrvuz.supabase.co/rest/v1/portfolio_posts?select=title,description,url,id,published_at&order=published_at.desc&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpZ2dtdmdtem9mZm5leWZydnV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc5MjE1NzYsImV4cCI6MTk4MzQ5NzU3Nn0.74Wy0yL0oWfA5M1koxzpswBUwAwFS8RMPpeugRaWVw4`,
+    `${process.env.NEXT_PUBLIC_SUPABASE_API_URL}/rest/v1/portfolio_posts?select=title,description,url,id,published_at&order=published_at.desc&apikey=${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
     40
   );
 }
