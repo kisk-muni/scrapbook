00578daa0bfef53a7f7e244369d9c015ac335194
cernockyd <24832873+cernockyd@users.noreply.github.com>
2022-11-15 16:05:30 +0100

    responsive hero

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ function Hero() {
   return (
     <div className="mx-auto mt-6 mb-8 px-4 sm:mt-6 sm:mb-16 sm:px-6 md:mt-8 md:mb-20">
       <div className="text-center justify-center">
-        <h1 className="text-4xl mb-3 tracking-tight text-transparent bg-gradient-header from-yellow to-orange bg-clip-text font-header sm:text-5xl md:text-6xl">
+        <h1 className="text-5xl mb-3 tracking-tight text-transparent bg-gradient-header from-yellow to-orange bg-clip-text font-header md:text-6xl lg:text-6xl xl:text-7xl">
           <span className="block xl:inline">KISKový Scrapbook</span>
         </h1>
         <p className="mt-0 text-lg md:text-xl lg:text-2xl text-text mx-auto">
