a661aec85a63b62e4c94a8eab18a751081f28ef9
cernockyd <24832873+cernockyd@users.noreply.github.com>
2022-11-21 07:47:05 +0100

    make button-help less prominent

--- app/add-portfolio/button-help.tsx
+++ app/add-portfolio/button-help.tsx
@@ -20,32 +20,9 @@ export default function ButtonHelp(props: ButtonProps) {
     <button
       {...mergeProps(buttonProps, focusProps)}
       ref={ref}
-      className={classNames(
-        'px-1',
-        'sm:px-2',
-        'sm:py-1',
-        'text-sm',
-        'text-background',
-        'cursor-pointer',
-        'rounded-r-md',
-        'bg-slate',
-        'border-2',
-        'border-slate',
-        'relative',
-        'inline-flex',
-        'flex-row',
-        'items-center',
-        'justify-center',
-        'rounded-md',
-        'overflow-hidden',
-        'shadow-sm',
-        'outline-none',
-        'hover:bg-purple hover:border-purple',
-        {
-          'ring-2 ring-offset-2 ring-slate bg-slate text-background':
-            isFocusVisible,
-        }
-      )}
+      className={classNames('cursor-pointer', 'hover:underline', {
+        'ring-2 ring-offset-2 ring-slate': isFocusVisible,
+      })}
     >
       {children}
     </button>
