9670aefdbee0e4d3100ef9f48800a9e1f19ef847
cernockyd <24832873+cernockyd@users.noreply.github.com>
2022-12-13 15:06:40 +0100

    promote discord

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,27 @@
 'use client';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Posts } from './posts';
+import Link from 'next/link';
+
+function Alert() {
+  return (
+    <div className="text-center lg:px-4 xl:-mt-12">
+      <Link target="_blank" href="https://discord.gg/HDacGhAhj8">
+        <div
+          className="py-1.5 sm:-my-4 xl:mb-4 px-1.5 bg-green cursor-pointer duration-500 hover:scale-105 items-center text-black leading-none rounded-full flex sm:inline-flex"
+          role="alert"
+        >
+          <span className="font-semibold mr-1.5 ml-1.5 text-left flex-auto">
+            Diskuze nad články probíhá na KISK Discordu
+          </span>
+          <span className="bg-black relative rounded-full -rotate-1 uppercase px-2 py-1 text-xs font-bold text-green text-center flex justify-between items-center">
+            získat pozvánku<span className="ml-1.5">&gt;</span>
+          </span>
+        </div>
+      </Link>
+    </div>
+  );
+}
 
 function Hero() {
   return (
@@ -20,6 +41,7 @@ function Hero() {
 export default function HomePage() {
   return (
     <div>
+      <Alert />
       <Hero />
       <ErrorBoundary fallback={<div>Příspěvky se nepodařilo načíst.</div>}>
         <Posts />
