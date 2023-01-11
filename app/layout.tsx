ff251f851cebf45010c60599e45213ff1a73cf92
cernockyd <24832873+cernockyd@users.noreply.github.com>
2023-01-11 10:03:37 +0100

    pwa icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,27 @@ export default function RootLayout({
         <title>KISK Scrapbook</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f37a28" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#da532c" />
       </head>
       <body className="bg-background px-4 md:px-6 lg:px-8">
         <header>
