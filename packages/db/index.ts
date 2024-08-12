e1c98f48d8b4dfef237e5b02f7992a64dba83a47
cernockyd <24832873+cernockyd@users.noreply.github.com>
2024-08-12 16:56:23 +0200

    disable drizzle prepare

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -2,15 +2,12 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import * as Schema from "db/schema";
 import postgres from "postgres";
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const queryClient = postgres({
-  idle_timeout: 20,
-  max_lifetime: 60 * 30,
-});
-
 if (!process.env.DB_URL) {
   throw new Error("DB_URL is required");
 }
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const queryClient = postgres(process.env.DB_URL, { prepare: false });
 
 export const db = drizzle(queryClient, {
   schema: {
