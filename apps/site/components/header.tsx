d2df89ff184d3e5e63ba0a99e7cfb58946d47546
cernockyd <24832873+cernockyd@users.noreply.github.com>
2024-10-22 13:50:56 +0200

    make navigation on mobile more friendly

--- apps/site/components/header.tsx
+++ apps/site/components/header.tsx
@@ -4,13 +4,15 @@ import { auth } from 'auth';
 import { UserMenu } from 'components/user-menu';
 import { LoginButton } from './login-button';
 import { Navigation } from './navigation';
+import Link from 'next/link';
+import Logo from './logo';
 
 async function UserOrLogin() {
   const session = await auth();
 
   return (
     <>
-      <div className="flex items-center">
+      <div className="flex lg:w-40 md:w-auto items-center justify-end">
         {session?.user ? <UserMenu user={session.user} /> : <LoginButton />}
       </div>
     </>
@@ -19,11 +21,20 @@ async function UserOrLogin() {
 
 export async function Header() {
   return (
-    <header className="fixed bottom-0 md:sticky md:bottom-[none] md:top-0 inset-x-0 z-30 flex items-center justify-between w-full h-16 px-4 md:px-6 lg:px-8 border-b border-sunken shrink-0 bg-background backdrop-blur-xl">
+    <header className="fixed bottom-0 md:sticky md:bottom-[none] md:top-0 inset-x-0 z-30 flex items-center justify-between w-full h-16 px-4 md:px-6 lg:px-8 border-t md:border-t-0 md:border-b border-sunken shrink-0 bg-background backdrop-blur-xl">
       <nav className="flex w-full justify-between items-center h-16">
-        <Navigation />
-        <div className="flex items-center">
-          <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
+        <div className="flex w-full items-center justify-between space-x-3 lg:space-x-6">
+          <Link
+            href="/"
+            className="hidden sm:flex lg:w-40 md:w-auto items-center text-xl pt-px text-muted hover:text-purple font-bold"
+          >
+            <Logo />
+            <span className="font-header hidden md:inline-block mt-0.5">
+              Scrapbook
+            </span>
+          </Link>
+          <Navigation />
+          <React.Suspense fallback={<div className=" overflow-auto" />}>
             <UserOrLogin />
           </React.Suspense>
         </div>
--- apps/site/components/navigation.tsx
+++ apps/site/components/navigation.tsx
@@ -10,47 +10,46 @@ import {
 } from './ui/icons';
 import { Button } from './ui/button-radix';
 import classNames from 'classnames';
+import { Fragment } from 'react';
 
 const navigation = [
-  { href: '/', label: 'Domů', icon: IconHouse },
+  { href: '/', label: 'Domů', classNames: '', icon: IconHouse },
   { href: '/students', label: 'Studenti', icon: IconUsersThree },
-  { href: '/analytics/posts', label: 'Analytika', icon: IconChartPie },
-  { href: '/about', label: 'O Scrapbooku', icon: IconQuestion },
+  {
+    href: '/analytics/posts',
+    label: 'Analytika',
+    classNames: 'hidden md:flex',
+    icon: IconChartPie,
+  },
+  {
+    href: '/about',
+    classNames: '',
+    label: 'O Scrapbooku',
+    icon: IconQuestion,
+  },
 ];
 
 export function Navigation() {
   const pathname = usePathname();
   return (
-    <>
-      <div className="hidden sm:flex items-center">
-        <Link
-          href="/"
-          className="text-xl pt-px flex items-center text-muted hover:text-purple font-bold"
-        >
-          <Logo />
-          <span className="font-header hidden md:inline-block mt-0.5">
-            Scrapbook
-          </span>
-        </Link>
-      </div>
-      <div className="flex grow items-baseline justify-center space-x-3 lg:space-x-6 mx-auto mr-8">
-        {navigation.map((item: any) => (
-          <Button asChild variant="ghost" key={item.href}>
-            <Link
-              href={item.href}
-              className={classNames(
-                'text-muted hover:text-slate hover:text-gray relative md:flex justify-center text-base font',
-                {
-                  'bg-white text-text hover:bg-white': pathname === item.href,
-                }
-              )}
-            >
-              <item.icon className="h-5 w-5 md:mr-1 -mt-0.5" />{' '}
-              <span className="hidden md:inline">{item.label}</span>
-            </Link>
-          </Button>
-        ))}
-      </div>
-    </>
+    <div className="flex justify-between w-full max-w-4xl">
+      {navigation.map((item: any) => (
+        <Button asChild variant="ghost" key={item.href}>
+          <Link
+            href={item.href}
+            className={classNames(
+              'text-muted hover:text-slate hover:text-gray rounded-xl relative md:flex justify-center text-base font',
+              {
+                'bg-white text-text hover:bg-white': pathname === item.href,
+              },
+              item?.classNames
+            )}
+          >
+            <item.icon className="h-5 w-5 md:mr-1 -mt-0.5" />{' '}
+            <span className="hidden md:inline">{item.label}</span>
+          </Link>
+        </Button>
+      ))}
+    </div>
   );
 }
